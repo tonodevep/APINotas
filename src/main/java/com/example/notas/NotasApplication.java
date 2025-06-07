20d7864a6b60d8cc382664ee688b32a604871301
Toño Tejera <tonodevep@gmail.com>
2025-06-07 14:52:26 +0100

    Añadido NotasApplication.java

---
+++ src/main/java/com/example/notas/NotasApplication.java
@@ -0,0 +1,12 @@
+package com.example.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotasApplication.class, args);
+    }
+}
+
